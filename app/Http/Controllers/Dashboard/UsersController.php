48b002b3e90bdabea50e983479372c6c98948c64
Mateus Vinicius <mateusvinicios7@gmail.com>
2019-05-05 19:49:39 -0300

    hoje

--- app/Http/Controllers/Dashboard/UsersController.php
+++ app/Http/Controllers/Dashboard/UsersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Dashboard;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
-
+use Illuminate\Support\Facades\Validator;
 class UsersController extends Controller
 {
     public function index(){
@@ -22,5 +22,25 @@ class UsersController extends Controller
     public function create(){
 
         return view('dashboard.pages.create_users');
+    }
+
+
+    public function store(Request $request){
+        return $this.validator($request->all())->validate();
+     
+
+      
+   
+    }
+
+
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'val-username2' => ['required', 'string', 'max:255'],
+            'val-email2' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'val-password2' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
     }
 }
--- resources/views/dashboard/pages/create_users.blade.php
+++ resources/views/dashboard/pages/create_users.blade.php
@@ -23,7 +23,9 @@
             <div class="col-xl-6">
                 <!-- jQuery Validation (.js-validation-material class is initialized in js/pages/be_forms_validation.js) -->
                 <!-- For more examples you can check out https://github.com/jzaefferer/jquery-validation -->
-                <form class="js-validation-material" action="be_forms_validation.html" method="post" novalidate="novalidate">
+            <form class="js-validation-material" action="{{route('users.store')}}" method="post" novalidate="novalidate">
+
+                @csrf
                     <div class="form-group">
                         <div class="form-material">
                             <input type="text" class="form-control" id="val-username2" name="val-username2" placeholder="Enter a username..">
@@ -50,8 +52,8 @@
                     </div>
                     <div class="form-group">
                         <div class="form-material">
-                            <select class="js-select2 form-control js-select2-enabled select2-hidden-accessible" id="val-select22" name="val-select22" style="width: 100%;" data-placeholder="Choose one.." tabindex="-1" aria-hidden="true">
-                                <option></option><!-- Required for data-placeholder attribute to work with Select2 plugin -->
+                            <select class="form-control" id="val-skill2" name="val-skill2" aria-describedby="val-skill2-error" aria-invalid="true">
+                                <option value="">Please select</option>
                                 <option value="html">HTML</option>
                                 <option value="css">CSS</option>
                                 <option value="javascript">JavaScript</option>
@@ -63,9 +65,10 @@
                                 <option value="asp">ASP.NET</option>
                                 <option value="python">Python</option>
                                 <option value="mysql">MySQL</option>
-                            </select><span class="select2 select2-container select2-container--default select2-container--below" dir="ltr" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-val-select22-container"><span class="select2-selection__rendered" id="select2-val-select22-container"><span class="select2-selection__placeholder">Choose one..</span></span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
-                            <label for="val-select2">Nivel</label>
+                            </select>
+                            <label for="val-skill2">Nivel</label>
                         </div>
+
                     </div>
                     
                     <div class="form-group">
@@ -77,4 +80,14 @@
     </div>
 </div>
 
+@endsection
+
+
+@section('sc')
+
+
+<script src="{{ asset('js/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('js/plugins/select2/select2.full.min.js') }}"></script>
+<script src="{{ asset('js/pages/be_forms_validation.js') }}"></script>
+<script src="{{ asset('js/plugins/jquery-validation/additional-methods.min.js') }}"></script>
 @endsection
